a01c61648f3952012b5771011cfb68f0013ddfb3
ugtakhbayar surakhbayar <ugtakhbayars@gmail.com>
2021-10-19 16:34:03 +0800

    Landing page text is correct.

--- wishlist_frontend/pages/index.tsx
+++ wishlist_frontend/pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>WishList Web Application</h1>
+        <h1 className={styles.title}>WishList Web Application - 2</h1>
       </main>
 
       <footer className={styles.footer}>Powered by Wish Team</footer>
