c00b6242577ef2d88900f52a9c679dae66b2778a
azzayabat <73649892+azzayabat@users.noreply.github.com>
2021-10-22 09:57:10 +0800

    Monita group page (#6)
    * yarn.lock
    
    * monita group page
    
    * modified mock data
    
    * changed monita group UI

--- wishlist_frontend/components/group-user-card.tsx
+++ wishlist_frontend/components/group-user-card.tsx
@@ -7,32 +7,33 @@ interface IUserCard {
 export const GroupUserCard = ({ props }: IUserCard) => {
   const { imageUrl, name, active } = props;
   return (
-    <div>
+    <div
+      className="user-card"
+      style={{
+        width: "100%",
+        height: "50px",
+        // border: "1px solid red",
+        boxShadow: "rgba(0, 0, 0, 0.1) 0px 4px 12px",
+        borderRadius: "25px",
+        display: "flex",
+        alignItems: "center",
+        margin: "5px",
+      }}
+    >
       <div
-        className="user-card"
+        className="user-card-avatar"
         style={{
-          width: "150px",
-          height: "200px",
-          border: active ? "2px solid #39ff14" : "2px solid #ff073a",
-          borderRadius: "15px",
-          margin: "10px",
           backgroundImage: `url("${imageUrl}")`,
+          width: "50px",
+          height: "50px",
+          borderRadius: "25px",
           backgroundSize: "cover",
           backgroundRepeat: "no-repeat",
           backgroundPosition: "center",
+          border: active ? "2px solid #39ff14" : "2px solid #ff073a",
         }}
-      ></div>
-      <div>
-        <p
-          style={{
-            textAlign: "center",
-            fontSize: "1.2em",
-            fontWeight: 600,
-          }}
-        >
-          {name}
-        </p>
-      </div>
+      />
+      <p style={{ marginLeft: "20px" }}>{name}</p>
     </div>
   );
 };
